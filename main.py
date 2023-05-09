f7484286850e524c751cc70695a9594219a1eec7
TheMAN1003 <fi.03maksimnedozhdii@gmail.com>
2023-05-10 01:12:44 +0300

    added smoothness check, triangle mod form changer, needs cleanup, gauss wip

--- main.py
+++ main.py
@@ -1,5 +1,13 @@
 from sympy.ntheory import factorint
 from math import exp, log2, sqrt
+import numpy as np
+
+def extended_gcd(a, b):
+    if b == 0:
+        return a, 1, 0
+    else:
+        (d, x, y) = extended_gcd(b, a % b)
+        return d, y, x - int(a / b) * y
 
 def powMod(x, pow, mod):
     C = 1
@@ -26,4 +34,62 @@ def buildBase(n):
                 nprime += 1
     return primelist
 
+def checkSmooth(alpha, base, mod, n):
+    listk = []
+    matrix = []
+    for k in range(0, n):
+        number = powMod(alpha, k, mod)
+        dividers = factorint(number)
+        for prime in dividers:
+            if prime not in base:
+                continue
+        listk.append(k)
+        if len(listk) > len(base) + 10:
+            break
+        matrix.append(dividers.values())
+    return matrix, listk
+
+def pivot(h, matrix):
+    max = 0
+    counter = 0
+    for i in range(h, len(matrix)):
+        if abs(matrix[i][h]) > max:
+            max = abs(matrix[i][h])
+            counter = i
+    return counter
+
+def triangle(matrix, vector, mod):
+    vector = np.array(vector)
+    matrix = np.array(matrix)
+    minlen = min(len(matrix), len(matrix[0]))
+    for k in range(minlen):
+        i_max = pivot(k, matrix)
+        if matrix[i_max][k] == 0:
+            print("Mistake in matrix")
+            return
+        matrix[[k, i_max]] = matrix[[i_max, k]]
+        vector[k], vector[i_max] = vector[i_max], vector[k]
+        for i in range(k+1, len(matrix)):
+            f = (matrix[i][k] * extended_gcd(matrix[k][k], mod)[1]) % mod
+            for j in range(k+1, len(matrix[0])):
+                matrix[i][j] = (matrix[i][j] - matrix[k][j]*f) % mod
+                vector[i] = (vector[i] - vector[k]*f) % mod
+            matrix[i][k] = 0
+    return matrix, vector
+
+# def Gauss(matrix, vector, mod):
+#     solution = []
+#     for i in reversed(range(len(matrix))):
+
+
+matrix = [[5, 7, 11], [12, 9, 1], [4, 13, 17]]
+vector = [4, 5, 1]
+
+print(triangle(matrix, vector, 37))
+
+
+a = 1
+b = 1
+mod = 10
+n = mod - 1
 
